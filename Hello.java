d9c231b1eef22bcc9c39db786e3dca6e7df995a0
sarvan4324 <sarvan4324@gmail.com>
2024-12-09 20:21:02 +0530

    updated README file and added Hello java file

---
+++ Hello.java
@@ -0,0 +1,8 @@
+public class Hello{
+     static String name="sarvan singh k";
+   public static void main(String[] arg){
+       System.out.println("hello "+name);
+       System.out.println("hope you doing good");
+   } 
+
+}
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # 2024_store
 This repository is for learning github  <br>
-Author - sarvan singh k
+Author - sarvan singh k <br>
+this line has been added
+
